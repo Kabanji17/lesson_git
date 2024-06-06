f9e02b753f7c971e90137a7ff4053f20fc46dc2e
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-06-06 16:19:12 +0430

    filter eng names

--- src/name.py
+++ src/name.py
@@ -31,6 +31,14 @@ def filter_russian_names(names_list: list) -> list:
     return new_names_list
 
 
+def filter_english_names(names_list: list) -> list:
+    """ Фильтрация имен написанных на английском """
+    new_names_list = list()
+    for name_item in names_list:
+        if not is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
 def save_to_file(file_name: str, data: str) -> None:
     """ Сохраняет данные в файл """
     with open('../data/' + file_name, 'w', encoding="utf-8") as names_file:
@@ -44,4 +52,10 @@ if __name__ == "__main__":
     save_to_file(
     'russian_names.txt',
     '\n'.join(filtered_names)
+    )
+
+    filtered_names = filter_english_names(cleared_names)
+    save_to_file(
+        'english_names.txt',
+        '\n'.join(filtered_names)
     )
